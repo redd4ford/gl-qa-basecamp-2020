a503f4b3bd30485ce1b55beee9bbede807a36c57
Viktoriia Yehorova <egorova.vik@gmail.com>
2022-11-11 17:41:52 +0200

    Changed the way to get WebDriver via webdriver-manager package

--- ui/config/config.py
+++ ui/config/config.py
@@ -1,11 +1,15 @@
 import os
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 class Config:
-    GECKO_DRIVER_PATH = os.getenv('GECKO_PATH', 'C:\\Program Files\\Mozilla Firefox\\geckodriver.exe')
-    DRIVER = webdriver.Firefox(executable_path=GECKO_DRIVER_PATH)
-    DEFAULT_TIMEOUT = 10
-    GLOBAL_LOGIC_BASE_URL = os.getenv('GL_URL', 'https://www.globallogic.com/ua')
-    GOOGLE_BASE_URL = os.getenv('GO_URL', 'https://www.google.com')
-    PYPI_URL = os.getenv('PYPI_URL', 'https://pypi.org')
+    # OLD WAY
+    # WEBDRIVER_PATH = os.getenv('GECKO_PATH', '/home/yehorova_v/personal/geckodriver')
+    # DRIVER = webdriver.Firefox(executable_path=WEBDRIVER_PATH)
+    # NEW WAY
+    DRIVER = webdriver.Chrome(ChromeDriverManager().install())
+    DEFAULT_TIMEOUT: int = 10
+    GLOBAL_LOGIC_BASE_URL: str = os.getenv('GL_URL', 'https://www.globallogic.com/ua')
+    GOOGLE_BASE_URL: str = os.getenv('GO_URL', 'https://www.google.com')
+    PYPI_BASE_URL: str = os.getenv('PYPI_URL', 'https://pypi.org')

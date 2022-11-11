dc108e1b4e42b762e67c4cff3f60b4c9b85952f8
Viktoriia Yehorova <egorova.vik@gmail.com>
2022-11-11 17:42:23 +0200

    Split Endpoints to different classes

--- ui/config/endpoints.py
+++ ui/config/endpoints.py
@@ -1,3 +1,7 @@
-class Endpoints:
-    GLOBAL_LOGIC_CAREERS_URL = '/careers'
-    GLOBAL_LOGIC_CAREERS_SEARCH_URL = '/career-search-page'
+class GlobalLogicEndpoints:
+    GLOBAL_LOGIC_CAREERS_URL: str = '/careers'
+    GLOBAL_LOGIC_CAREERS_SEARCH_URL: str = '/career-search-page'
+
+
+class PyPIEndpoints:
+    PY_PI_PROJECT_URL: str = '/project'
